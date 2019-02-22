713d147609ba3efdcc7b07d98185c8b4d3995a09
Nunez Suarez <000400886@bga.upb>
2019-02-22 10:51:46 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Calcular el producto de dos numeros enteros")
+
+a=int(input("Digita un numero, Por favor: "))
+c=int(input("Digita un numero, Por favor: "))
+
+producto=a*c
+producto_de_a=a*2
+
+print(producto)
+print(producto_de_a)
+
+print("Se ha calculado con exito")
