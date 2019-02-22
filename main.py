f6712af085ffc74d2abea07be4f84d2932fdbcfa
Nunez Suarez <000400886@bga.upb>
2019-02-22 11:26:41 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+
 print("Calcular el producto de dos numeros enteros")
 
 a=int(input("Digita un numero, Por favor: "))
+
 c=int(input("Digita un numero, Por favor: "))
 
 producto=a*c
+
 producto_de_a=a*2
 
 print(producto)
+
 print(producto_de_a)
+
+print("Se ha calculado con exito")
+
+import math
+
+print("Calcular una potencia de un numero b y una raiz de un numero d")
+
+b=int(input("Digita un numero, Por favor: "))
+
+d=int(input("Digita un numero, Por favor: "))
+
+potencia=pow(b,2)
+
+print("La potencia es " + str(potencia))
+
+raiz=math.sqrt(d)
+
+print("La raiz es " + str(raiz))
 
 print("Se ha calculado con exito")
