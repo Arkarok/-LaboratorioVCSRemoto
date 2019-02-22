d7b806ffc135d909e752dcb02f4696e3526ec2b0
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-02-22 18:27:46 -0500

    Solución

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 print("Calcular el producto de dos numeros enteros")
 
 a=int(input("Digita un numero, Por favor: "))
-
 c=int(input("Digita un numero, Por favor: "))
 
 producto=a*c
@@ -20,7 +19,6 @@ import math
 print("Calcular una potencia de un numero b y una raiz de un numero d")
 
 b=int(input("Digita un numero, Por favor: "))
-
 d=int(input("Digita un numero, Por favor: "))
 
 potencia=pow(b,2)
@@ -32,4 +30,28 @@ raiz=math.sqrt(d)
 print("La raiz es " + str(raiz))
 
 print("Se ha calculado con exito")
+
+print("Funcion Cuadratica")
+
+import math
+
+a=int(input('Ingrese el valor de a '))
+
+if a==0:
+    print('El valor de "a" no puede ser cero ')
+    print('reingrese el valor de "a" ')
+    
+    a=int(input('Ingrese el valor de a: '))
+b=int(input('Ingrese el valor de b: '))
+c=int(input('Ingrese el valor de c: '))
+
+x1=(-b+(b**2-4*a*c)**(1/2))/(2*a)
+x2=(-b-(b**2-4*a*c)**(1/2))/(2*a)
+
+print(x1)
+print(x2)
+
+print("Se ha calculado con exito")
+
+
 
